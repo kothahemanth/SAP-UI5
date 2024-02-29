66450d998d0d972bfefad48717ff6b5abcac4531
Kotha Hemanth <hemanthkotha17@gmail.com>
2024-02-29 14:56:18 +0530

    SAP-UI5

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.App", {
+
+        
+		onShowHello() {
+			// show a native JavaScript alert
+			/* eslint-disable no-alert */
+			alert("Hello World");
+			/* eslint-enable no-alert */
+		},
+        onShowHelloo() {
+            MessageToast.show("Hello World");
+         },
+
+         onInit() {
+            // set data model on view
+            const oData = {
+               recipient : {
+                  name : ""
+               }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+
+        // set i18n model on view
+         const i18nModel = new ResourceModel({
+            bundleName: "ui5.walkthrough.i18n.i18n"
+         });
+         this.getView().setModel(i18nModel, "i18n");
+      },
+
+      onShowHello() {
+         // read msg from i18n model
+         const oBundle = this.getView().getModel("i18n").getResourceBundle();
+         const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+         // show message
+         MessageToast.show(sMsg);
+
+         }
+	});
+});
---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/core/ComponentContainer"
+], (XMLView, ComponentContainer) => {
+	"use strict";
+	XMLView.create({
+		viewName: "ui5.walkthrough.view.App"
+	}).then((oView) => oView.placeAt("content"));
+	new ComponentContainer({
+		name: "ui5.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
+});
