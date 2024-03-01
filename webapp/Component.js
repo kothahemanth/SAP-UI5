94d0c65b1ac455b6ca3ffacd53ea83ed8b51dfcf
Kotha Hemanth <hemanthkotha17@gmail.com>
2024-03-01 17:26:03 +0530

    step-36

--- webapp/Component.js
+++ webapp/Component.js
@@ -32,6 +32,9 @@ sap.ui.define([
 			// create the views based on the url/hash
 			this.getRouter().initialize();
 
+		},
+		getContentDensityClass() {
+			return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
 		}
 	});
 });
